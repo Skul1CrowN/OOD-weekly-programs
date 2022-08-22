2fc61a3a125ad37b086e3a079bded03599937665
Skul1CrowN <boat.72545@gmail.com>
2022-08-22 15:05:10 +0700

    Color crush 2 solved

--- 4_Queue/5.py
+++ 4_Queue/5.py
@@ -1,6 +1,6 @@
 class Queue:
-    def __init__(self, data=[]):
-        self.items = list(data)
+    def __init__(self):
+        self.items = []
 
     def enqueue(self, data):
         self.items.append(data)
@@ -28,9 +28,12 @@ class Queue:
     def size(self):
         return len(self.items)
 
+    def reverse(self):
+        self.items.reverse()
+
 class Stack:
-    def __init__(self,data=[]):
-        self.items = data
+    def __init__(self):
+        self.items = []
 
     def push(self,data):
         self.items.append(data)
@@ -52,3 +55,83 @@ class Stack:
 
     def size(self):
         return len(self.items)
+
+    def reverse(self):
+        self.items.reverse()
+
+normal, mirror = input('Enter Input (Normal, Mirror) : ').split(' ')
+mirror = mirror[::-1]
+mirrorExplosion = 0
+
+mirrorStack = Stack()
+mirrorItemQueue = Queue()
+
+for i in mirror:
+    if mirrorStack.size() < 2:
+        mirrorStack.push(i)
+    else:
+        mirrorStack.push(i)
+        third, second, first = mirrorStack.pop(), mirrorStack.pop(), mirrorStack.pop()
+        if not third == second == first:
+            mirrorStack.push(first)
+            mirrorStack.push(second)
+            mirrorStack.push(third)
+        else:
+            mirrorItemQueue.enqueue(first)
+            mirrorExplosion += 1
+
+normalStack = Stack()
+fail, normalExplosion, count = 0,0,0
+
+for i in normal:
+    if normalStack.size() < 2:
+        if normalStack.size() == 1 and i == normalStack.top():
+            count = 2
+        else:
+            count = 0
+        normalStack.push(i)
+    elif count == 2 and not mirrorItemQueue.isEmpty():
+        count = 0
+        front = mirrorItemQueue.dequeue()
+        normalStack.push(front)
+        third, second, first = normalStack.pop(), normalStack.pop(), normalStack.pop()
+        if third == second == first:
+            fail += 1
+        else:
+            normalStack.push(first)
+            normalStack.push(second)
+            normalStack.push(third)
+        normalStack.push(i)
+    else:
+        if i == normalStack.top():
+            count = 2
+        else:
+            count = 0
+        normalStack.push(i)
+        third, second, first = normalStack.pop(), normalStack.pop(), normalStack.pop()
+        if third == second == first:
+            normalExplosion += 1
+        else:
+            normalStack.push(first)
+            normalStack.push(second)
+            normalStack.push(third)
+
+print('NORMAL : ')
+print(normalStack.size())
+normalStack.reverse()
+if normalStack.isEmpty():
+    print('Empty')
+else:
+    print(''.join(normalStack.items))
+print(normalExplosion, 'Explosive(s) ! ! ! (NORMAL)')
+if fail > 0:
+    print('Failed Interrupted',fail,'Bomb(s)')
+print("------------MIRROR------------")
+print(": RORRIM")
+print(mirrorStack.size())
+mirrorStack.reverse()
+if mirrorStack.isEmpty():
+    print('ytpmE')
+else:
+    print(''.join(mirrorStack.items))
+print('(RORRIM) ! ! ! (s)evisolpxE', mirrorExplosion)
